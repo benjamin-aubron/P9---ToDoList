93b28d16ea6415052dda99aa5b5fd675aa977765
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-09-23 18:03:05 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+
+  return (
+    <div className='min-h-screen bg-slate-700'>
+      <h1 className="text-3xl text-center p-4 text-slate-200">My To Do List</h1>
+    </div>
+  )
+}
+
+export default App
